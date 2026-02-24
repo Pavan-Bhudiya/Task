dc12e3548448aaadd46249c1968b8f037442430a
Pavan-Bhudiya <pavanbhudiya@gmail.com>
2026-02-24 17:18:49 +0300

    implemented transaction creation and validation

--- backend/app/Http/Controllers/Api/TransactionController.php
+++ backend/app/Http/Controllers/Api/TransactionController.php
@@ -3,9 +3,22 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Transaction;
 use Illuminate\Http\Request;
 
 class TransactionController extends Controller
 {
-    //
+    public function store(Request $request)
+    {
+        $request->validate([
+            'wallet_id' => 'required|exists:wallets,id',
+            'amount' => 'required|numeric|min:1',
+            'type' => 'required|in:income,expense',
+            'description' => 'nullable|string'
+        ]);
+
+        $transaction = Transaction::create($request->all());
+
+        return response()->json($transaction);
+    }
 }
