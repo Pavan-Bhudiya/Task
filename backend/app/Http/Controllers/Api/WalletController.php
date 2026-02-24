6dacd80046d93636268173236bff036008821777
Pavan-Bhudiya <pavanbhudiya@gmail.com>
2026-02-24 15:31:40 +0300

    implemented wallet endpoint

--- backend/app/Http/Controllers/Api/WalletController.php
+++ backend/app/Http/Controllers/Api/WalletController.php
@@ -3,9 +3,31 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Wallet;
 use Illuminate\Http\Request;
 
 class WalletController extends Controller
 {
-    //
+    public function store(Request $request)
+    {
+        $request->validate([
+            'user_id' => 'required|exists:users,id',
+            'name' => 'required'
+        ]);
+
+        $wallet = Wallet::create($request->all());
+
+        return response()->json($wallet);
+    }
+
+    public function show($id)
+    {
+        $wallet = Wallet::with('transactions')->findOrFail($id);
+
+        return response()->json([
+            'wallet' => $wallet->name,
+            'balance' => $wallet->balance(),
+            'transactions' => $wallet->transactions
+        ]);
+    }
 }
