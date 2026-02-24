de774967c5ceb5b2cc37428c96c06223db321a10
Pavan-Bhudiya <pavanbhudiya@gmail.com>
2026-02-24 15:27:11 +0300

    added transaction model relationship

--- backend/app/Models/Transaction.php
+++ backend/app/Models/Transaction.php
@@ -3,8 +3,19 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Wallet;
 
 class Transaction extends Model
 {
-    //
+    protected $fillable = [
+        'wallet_id',
+        'amount',
+        'type',
+        'description'
+    ];
+
+    public function wallet()
+    {
+        return $this->belongsTo(Wallet::class);
+    }
 }
